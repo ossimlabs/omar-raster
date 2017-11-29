63aa6b931fa1b39210c120ef64c8020a598ad6d8
Stephen L'Allier <stephen.lallier@digitalglobe.com>
2017-11-29 14:33:53 -0500

    remove unnecessary printlns, old code

--- plugins/omar-raster-plugin/grails-app/services/omar/raster/RasterDataSetService.groovy
+++ plugins/omar-raster-plugin/grails-app/services/omar/raster/RasterDataSetService.groovy
@@ -3,7 +3,6 @@ package omar.raster
 import omar.core.Repository
 import omar.core.HttpStatus
 
-//import omar.stager.DataManagerService
 import omar.stager.OmarStageFile
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
@@ -44,8 +43,6 @@ class RasterDataSetService implements ApplicationContextAware {
 		String scheme = uri.scheme?.toLowerCase()
 		def raster_logs
 
-		log.info "got to add raster"
-
 		if(!scheme || (scheme=="file"))
 		{
 			File testFile = filename as File
@@ -79,12 +76,6 @@ class RasterDataSetService implements ApplicationContextAware {
 
 				httpStatusMessage.status = result.status
 				httpStatusMessage.message = result.message
-				//log.info( "submitting ${ filename } for background processing" )
-
-				//httpStatusMessage?.message = "submitting ${ filename } for background processing".toString()
-
-				//DataManagerQueueItem.addItem( [ file: "${ filename }", dataManagerAction: "addRaster" ],
-				//		true );
 			}
 			else {
 				def parser = parserPool?.borrowObject()
@@ -94,12 +85,12 @@ class RasterDataSetService implements ApplicationContextAware {
 
 				if(params.buildOverviews||params.buildHistograms) {
 					def result = stagerService.stageFileJni([filename:params.filename,
-																		  buildOverviews: params.buildOverviews,
-																		  buildHistograms:params.buildHistograms,
-																		  overviewCompressionType: params.overviewCompressionType,
-																		  buildHistogramsWithR0: params.buildHistogramsWithR0,
-																		  useFastHistogramStaging: params.useFastHistogramStaging,
-																		  overviewType: params.overviewType
+						  buildOverviews: params.buildOverviews,
+						  buildHistograms:params.buildHistograms,
+						  overviewCompressionType: params.overviewCompressionType,
+						  buildHistogramsWithR0: params.buildHistogramsWithR0,
+						  useFastHistogramStaging: params.useFastHistogramStaging,
+						  overviewType: params.overviewType
 					])
 					if(result?.status >= 300) { log.error(result?.message) }
 					httpStatusMessage.status = result.status
@@ -122,7 +113,6 @@ class RasterDataSetService implements ApplicationContextAware {
 								if (rasterDataSet.save()) {
 									//stagerHandler.processSuccessful(filename, xml)
 									httpStatusMessage?.status = HttpStatus.OK
-									log.info(httpStatusMessage?.message)
 									def ids = rasterDataSet?.rasterEntries.collect { it.id }.join(",")
 									httpStatusMessage?.message = "Added raster ${ids}:${filename}"
 									def missionids = rasterDataSet?.rasterEntries.collect { it.missionId }.join(",")
@@ -137,10 +127,6 @@ class RasterDataSetService implements ApplicationContextAware {
 											ingestDates: ingestDates, missionids: missionids, imageids: imageids, sensorids: sensorids)
 
 									log.info raster_logs.toString()
-
-
-
-
 								}
 								else {
 									savedRaster = false
@@ -155,99 +141,17 @@ class RasterDataSetService implements ApplicationContextAware {
 								log.error(httpStatusMessage?.message)
 							}
 						}
-						//new org.ossim.omar.DataManagerQueueItem(file: filename, baseDir: parent.baseDir, dataInfo: xml).save()
 					}
 				}
 			}
 		}
 
-
       httpStatusMessage
 	}
-
-/*
-def updateRaster(def httpStatusMessage, def params)
-{
-   if ( params.id )
-   {
-      def rasterEntry = RasterEntry.get(params.id)
-
-      if ( rasterEntry )
-      {
-         def omsInfoParser = applicationContext.getBean("rasterInfoParser")
-         def dataInfo = new DataInfo()
-         def canOpen = dataInfo.open(rasterEntry.mainFile?.name)
-
-         if ( canOpen )
-         {
-            def xml = dataInfo.getImageInfo(rasterEntry.entryId as int)?.trim()
-            dataInfo.close()
-            if ( xml )
-            {
-               def parser = parserPool.borrowObject()
-               def oms = new XmlSlurper(parser).parseText(xml)
-               parserPool.returnObject(parser)
-               RasterEntry.initRasterEntry(oms?.dataSets?.RasterDataSet?.rasterEntries?.RasterEntry, rasterEntry)
-               rasterEntry.save()
-            }
-         }
-      }
-      else
-      {
-         httpStatusMessage.message = "Query could not find record id  ${params.id} to update"
-         httpStatusMessage.status = HttpStatus.NOT_FOUND
-      }
-   }
-   else if ( params.filename )
-   {
-      def result = RasterDataSet.createCriteria().list {
-         fileObjects {
-            and {
-               eq('type', "main")
-               like('name', "%${params.filename}%")
-            }
-         }
-      }
-      if ( result.size() > 0 )
-      {
-         result.each {dataset ->
-            dataset.fileObjects.each {fileObject ->
-               dataset.rasterEntries.each {rasterEntry ->
-                  def dataInfo = new DataInfo()
-                  def canOpen = dataInfo.open(fileObject.name)
-
-                  if ( canOpen )
-                  {
-                     def xml = dataInfo.getImageInfo(rasterEntry.entryId as int)?.trim()
-                     dataInfo.close()
-                     dataInfo = null;
-                     if ( xml )
-                     {
-                        def parser = parserPool.borrowObject()
-                        def oms = new XmlSlurper(parser).parseText(xml)
-                        parserPool.returnObject(parser)
-                        rasterEntry.initRasterEntry(oms?.dataSets?.RasterDataSet?.rasterEntries?.RasterEntry)
-                        rasterEntry.save()
-                     }
-                  }
-               }
-            }
-         }
-      }
-      else
-      {
-         httpStatusMessage.message = "Query could not find file  ${params.filename} to update"
-         httpStatusMessage.status = HttpStatus.NOT_FOUND
-      }
-   }
-}
-*/
 
 	def removeRaster( def httpStatusMessage, def params ) {
 		def status = false
 		String filename = params?.filename //as File
-
-		log.info "got to remove raster"
 
 		def rasterFile = RasterFile.findByNameAndType( filename, "main" )
 		if ( rasterFile ) {
@@ -255,8 +159,7 @@ def updateRaster(def httpStatusMessage, def params)
 			httpStatusMessage?.status = HttpStatus.OK
 			def ids = rasterFile?.rasterDataSet?.rasterEntries?.collect { it?.id }?.join( "," )
 			httpStatusMessage?.message = "removed raster ${ ids }:${ filename }"
-			log.info( httpStatusMessage?.message )
-
+			
 			if (params.deleteFiles?.toBoolean()) {
 				def files = []
 				rasterFile?.rasterDataSet?.fileObjects.each() { files << it.name }
@@ -272,13 +175,28 @@ def updateRaster(def httpStatusMessage, def params)
 					{
 						File fileToRemove = file as File
 						if (fileToRemove.canWrite()) {
-							if (fileToRemove.isDirectory()) { fileToRemove.deleteDir() }
-							else { fileToRemove.delete() }
-							if (!fileToRemove.exists()) { log.info("Deleted ${file}") }
-							else { log.info("Unable to delete ${file}") }
+							if (fileToRemove.isDirectory())
+							{
+								fileToRemove.deleteDir() 
+							}
+							else 
+							{ 
+								fileToRemove.delete() 
+							}
+							if (!fileToRemove.exists()) 
+							{ 
+								log.info("Deleted ${file}") 
+							}
+							else 
+							{ 
+								log.info("Unable to delete ${file}") 
+							}
 						}
 					}
-					else { log.info("Don't have permissions to delete ${file}") }
+					else 
+					{ 
+						log.info("Don't have permissions to delete ${file}") 
+					}
 				}
 			}
 		}
@@ -312,41 +230,22 @@ def updateRaster(def httpStatusMessage, def params)
 			result.pagination.count = OmarStageFile.count()
 			result.pagination.offset = cmd.offset?:0
 			Integer limit = cmd.limit?:result.pagination.count
-			def files
-//			println "FILTER === ${cmd.filter}"
-//			def w = 'WHAT'
-//			def query = OmarStageFile.where {
-//				status=='READY'
-//			}
-
-//			if(!cmd.filter)
-//			{
-//				files = OmarStageFile.list([offset:result.pagination.offset, max:limit])
-				files = OmarStageFile.list([offset:result.pagination.offset, max:limit])
-//			}
-//			else
-//			{
-//		       files = query.find([offset:result.pagination.offset, max:limit])
-
-//			println files
-//				 files = OmarStageFile
-//			}
-			//files.list([offset:result.pagination.offset, max:limit]).each{record->
+			def files = OmarStageFile.list([offset:result.pagination.offset, max:limit])
 			files?.each{record->
 				result.results <<
-						[
-								filename:record.filename,
-								processId: record.processId,
-								status: record.status.name,
-								statusMessage: record.statusMessage,
-								buildOverviews: record.buildOverviews,
-								buildHistograms: record.buildHistograms,
-								buildHistogramsWithR0: record.buildHistogramsWithR0,
-								useFastHistogramStaging: record.useFastHistogramStaging,
-								overviewCompressionType: record.overviewCompressionType,
-								overviewType: record.overviewType,
-								dateCreated: record.dateCreated,
-						]
+					[
+						filename:record.filename,
+						processId: record.processId,
+						status: record.status.name,
+						statusMessage: record.statusMessage,
+						buildOverviews: record.buildOverviews,
+						buildHistograms: record.buildHistograms,
+						buildHistogramsWithR0: record.buildHistogramsWithR0,
+						useFastHistogramStaging: record.useFastHistogramStaging,
+						overviewCompressionType: record.overviewCompressionType,
+						overviewType: record.overviewType,
+						dateCreated: record.dateCreated,
+					]
 			}
 
 			result.pagination.limit = limit
