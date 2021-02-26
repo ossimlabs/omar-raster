d620a8d23b0e9858dafb74067714ee58dcfd0bc1
Craig Schneider <craig.schneider@maxar.com>
2021-02-26 10:18:42 -0500

    add spoc unit test

---
+++ plugins/omar-raster-plugin/src/test/groovy/omar/raster/RasterDataSetControllerSpec.groovy
@@ -0,0 +1,12 @@
+package omar.raster
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class RasterDataSetControllerSpec extends Specification implements ControllerUnitTest<RasterDataSetController> {
+    def "GetRasterFilesProcessing"() {
+    }
+
+    def "GetRasterFiles"() {
+    }
+}
