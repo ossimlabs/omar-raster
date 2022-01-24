70db2e77473a1532e9b1ebf788591b47eb96038f
sbortman <scott.bortman@gmail.com>
2022-01-24 14:28:15 -0500

    added HACK to override geometry from the omd file

--- plugins/omar-raster-plugin/grails-app/domain/omar/raster/RasterEntry.groovy
+++ plugins/omar-raster-plugin/grails-app/domain/omar/raster/RasterEntry.groovy
@@ -23,6 +23,9 @@ import omar.raster.tags.TargetIdTag
 
 import groovy.util.logging.Slf4j
 
+import org.apache.commons.io.FilenameUtils
+
+
 @Slf4j
 class RasterEntry
 {
@@ -409,6 +412,7 @@ class RasterEntry
       rasterEntry.gsdY = ( dy != "nan" ) ? dy?.toDouble() : null
       rasterEntry.gsdUnit = gsdUnit
     }
+    
     rasterEntry.groundGeom = initGroundGeom( rasterEntryNode?.groundGeom )
     rasterEntry.acquisitionDate = initAcquisitionDate( rasterEntryNode )
 
@@ -456,6 +460,25 @@ class RasterEntry
     { 
       rasterEntry.filename = filename
     }
+
+    /* HACK ALERT - START */
+    def omdFile = "${FilenameUtils.removeExtension( filename )}.omd" as File
+
+    if ( omdFile?.exists() ) {      
+        def kwl = omdFile?.readLines()?.inject([:]) { a, b ->
+          def c = b.split( ':' )?.collect { it.trim() }
+
+          a[c[0]] = c[1]
+          a
+        }
+
+        if ( kwl["ground_geom_${rasterEntry?.entryId}"]  ) {          
+          rasterEntry?.groundGeom = new WKTReader().read( kwl["ground_geom_${rasterEntry?.entryId}"]  )
+        }               
+
+    }    
+    /* HACK ALERT - END */
+
     initRasterEntryMetadata( metadataNode, rasterEntry )
 
     if(rasterEntry?.grailsApplication?.config?.stager?.includeOtherTags)
